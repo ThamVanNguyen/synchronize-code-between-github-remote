ef2052e4376f3b37f446df08215a9a8f2159431d
Tham Nguyen Van <nguyenvanthamtclc@gmail.com>
2021-11-17 19:26:32 +0700

    [SCGHR-5] Add column for entity

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -42,6 +42,9 @@ public class User
     @Column(length = 50)
     private String nickname;
 
+    @Column(length = 50)
+    private String fullName;
+
     @Column(length = 10)
     @Enumerated(EnumType.STRING)
     private Gender gender;
